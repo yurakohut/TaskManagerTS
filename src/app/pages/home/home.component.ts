5cb0d47e9a4a6c84075f7dc283181634f96c3c41
yurakohut <yurakohut97@gmail.com>
2020-03-27 10:53:00 +0200

    removed particle-js

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,35 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  myStyle: object = {};
-  myParams: object = {};
-  width: number = 100;
-  height: number = 100;
-
   ngOnInit() {
-    this.myStyle = {
-      'position': 'fixed',
-      'width': '100%',
-      'height': '100%',
-      'z-index': 1,
-      'top': '0',
-      'left': 0,
-      'right': 0,
-      'bottom': 0,
-    };
 
-    this.myParams = {
-      particles: {
-        number: {
-          value: 50,
-        },
-        color: {
-          value: '#050505'
-        },
-        shape: {
-          type: 'triangle',
-        },
-      }
-    };
   }
 }
